b81c97c1c3023b266b56ca989d9d8741f4535c7f
Michael Reilly <mikeyreilly@gmail.com>
2025-03-02 09:48:55 +0000

    use --enable-preview
    I wanted to try valhalla. It works with these java/javac flags I
    added. But using the value keyword makes intellij crap itself. So I
    won't be committing any such changes, but I will leave these options
    in place so I can mess around with valhalla easily.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf target/classes
-javac  -d target/classes --source-path src/main/java src/main/java/com/quaxt/mcc/Mcc.java
+javac --enable-preview --source 23 -g  -d target/classes --source-path src/main/java src/main/java/com/quaxt/mcc/Mcc.java
