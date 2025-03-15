1dbd1cc6e9e1e2ec7d22834a6fecab3950a4b641
Michael Reilly <mikeyreilly@gmail.com>
2025-03-15 11:43:24 +0000

    ch 15: codegen wip

--- src/main/java/com/quaxt/mcc/tacky/AddPtr.java
+++ src/main/java/com/quaxt/mcc/tacky/AddPtr.java
@@ -1,4 +1,4 @@
 package com.quaxt.mcc.tacky;
 
-public record AddPtr(ValIr ptr, ValIr index, int scale, ValIr dst ) implements InstructionIr {
+public record AddPtr(ValIr ptr, ValIr index, int scale, ValIr dst) implements InstructionIr {
 }
