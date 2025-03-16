02c2dc0fbec32a58d4233abb53ca764baeefece6
Michael Reilly <mikeyreilly@gmail.com>
2025-03-16 09:52:39 +0000

    ch 15: update depseudo

--- src/main/java/com/quaxt/mcc/asm/TypeAsm.java
+++ src/main/java/com/quaxt/mcc/asm/TypeAsm.java
@@ -3,6 +3,10 @@ package com.quaxt.mcc.asm;
 public sealed interface TypeAsm permits PrimitiveTypeAsm, ByteArray {
 
     int size();
+
+    default int alignment() {
+        return size();
+    }
 }
 
 
