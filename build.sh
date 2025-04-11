54d847685a5b2137c728e27e9423f27a6f217bbe
Michael Reilly <mikeyreilly@gmail.com>
2025-04-11 08:36:39 +0100

    jdk 24

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf target/classes
-javac --enable-preview --source 23 -g  -d target/classes --source-path src/main/java src/main/java/com/quaxt/mcc/Mcc.java
+javac --enable-preview --source 24 -g  -d target/classes --source-path src/main/java src/main/java/com/quaxt/mcc/Mcc.java
