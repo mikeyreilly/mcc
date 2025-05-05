bf9d2ea04158f5ae486bbd10b0ca3acf105f2d8f
Michael Reilly <mikeyreilly@gmail.com>
2025-05-05 15:07:08 +0100

    wip

--- src/main/java/com/quaxt/mcc/optimizer/EliminateDeadStores.java
+++ src/main/java/com/quaxt/mcc/optimizer/EliminateDeadStores.java
@@ -7,6 +7,17 @@ import java.util.*;
 
 public class EliminateDeadStores {
 
+    /**
+     * based on rewriteInstructions p. 598
+     */
+    public static boolean eliminateDeadStores(List<Node> cfg, Set<VarIr> aliasedVars) {
+        HashMap<Integer, ArrayList<HashSet<Copy>>> INSTRUCTION_ANNOTATIONS = new HashMap<>();
+        HashMap<Integer, HashSet<VarIr>> BLOCK_ANNOTATIONS = new HashMap<>();
+        iterativeAlgorithm(cfg, INSTRUCTION_ANNOTATIONS, BLOCK_ANNOTATIONS, aliasedVars);
+        boolean updated = false;
+        return updated;
+    }
+
     /* the iterative algorithm described on p. 607*/
     private static void iterativeAlgorithm(List<Node> cfg, HashMap<Integer, ArrayList<HashSet<Copy>>> INSTRUCTION_ANNOTATIONS, HashMap<Integer, HashSet<VarIr>> blockAnnotations, Set<VarIr> aliasedVars) {
         HashSet<VarIr> liveVars = new HashSet<>();
@@ -21,16 +32,17 @@ public class EliminateDeadStores {
         while (!workList.isEmpty()) {
             BasicBlock block = workList.removeFirst();
             HashSet<VarIr> oldAnnotations = getBlockAnnotation(block.nodeId(), blockAnnotations);
-            Set<Copy> incomingLiveVars = meet(block, liveVars, blockAnnotations);
+            Set<VarIr> incomingLiveVars = meet(block, blockAnnotations, aliasedVars);
             transfer(block, incomingLiveVars, INSTRUCTION_ANNOTATIONS, blockAnnotations, aliasedVars);
             if (!oldAnnotations.equals(getBlockAnnotation(block.nodeId(), blockAnnotations))) {
-                for (Node succ : block.successors()) {
-                    switch (succ) {
+                for (Node pred : block.predecessors()) {
+                    switch (pred) {
                         case BasicBlock basicBlock -> {
                             if (!workList.contains(basicBlock))
                                 workList.add(basicBlock);
                         }
-                        case EntryNode _ -> throw new Err("Malformed control flow graph");
+                        case EntryNode _ ->
+                                throw new Err("Malformed control flow graph");
                         case ExitNode _ -> {}
                     }
                 }
@@ -47,14 +59,6 @@ public class EliminateDeadStores {
         blockAnnotations.put(nodeId, liveVars);
     }
 
-    /**
-     * based on rewriteInstructions p. 598
-     */
-    public static boolean eliminateDeadStores(List<Node> cfg, Set<VarIr> aliasedVars) {
-
-        return false;
-    }
-
 
     private static Set<Copy> getInstructionAnnotation(int nodeId, int i, HashMap<Integer, ArrayList<HashSet<Copy>>> INSTRUCTION_ANNOTATIONS) {
         return INSTRUCTION_ANNOTATIONS.get(nodeId).get(i);
@@ -63,12 +67,28 @@ public class EliminateDeadStores {
 
     /*
     See p. 606 */
-    private static void transfer(BasicBlock block, Set<Copy> initialReachingCopies, HashMap<Integer, ArrayList<HashSet<Copy>>> INSTRUCTION_ANNOTATIONS, HashMap<Integer, HashSet<VarIr>> BLOCK_ANNOTATIONS, Set<VarIr> aliasedVars) {
+    private static void transfer(BasicBlock block, Set<VarIr> endLiveVars, HashMap<Integer, ArrayList<HashSet<Copy>>> INSTRUCTION_ANNOTATIONS, HashMap<Integer, HashSet<VarIr>> BLOCK_ANNOTATIONS, Set<VarIr> aliasedVars) {
+//        HashSet<VarIr> currentLiveVars = new HashSet<>(endLiveVars);
+//       var foo= new ArrayList<>();
+//       foo.ensureCapacity(1000);
+//        for(var instr:)
     }
 
     /* p. 607 */
-    private static Set<Copy> meet(BasicBlock block, HashSet<VarIr> allCopies, HashMap<Integer, HashSet<VarIr>> BLOCK_ANNOTATIONS) {
-        return null;
+    private static Set<VarIr> meet(BasicBlock block, HashMap<Integer, HashSet<VarIr>> BLOCK_ANNOTATIONS, Set<VarIr> aliasedVars) {
+        Set<VarIr> liveVars = new HashSet<>();
+        for (var succ : block.successors()) {
+            switch (succ) {
+                case BasicBlock _ -> {
+                    HashSet<VarIr> succLiveVars = getBlockAnnotation(succ.nodeId(), BLOCK_ANNOTATIONS);
+                    liveVars.addAll(succLiveVars);
+                }
+                case EntryNode _ ->
+                        throw new Err("Malformed control-flow graph");
+                case ExitNode _ -> liveVars.addAll(aliasedVars);
+            }
+        }
+        return liveVars;
     }
 
 
